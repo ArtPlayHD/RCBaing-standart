11abc052e8d2e6d659c9f1a3ae04ba556636441f
ArtPlayHD <artplay26f@yandex.ru>
2026-08-18 21:45:35 +0300

    Add files via upload

---
+++ Your_page.js
@@ -0,0 +1,7 @@
+document.getElementById("Name").innerHTML = localStorage.getItem('Name');
+document.getElementById("Mail").innerHTML = localStorage.getItem('Mail');
+document.getElementById("Phone number").innerHTML = localStorage.getItem('PN')+;
+function clearData(){
+	document.getElementById('myDialogClear').show()
+	
+}
